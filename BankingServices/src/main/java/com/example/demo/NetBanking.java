14d85c0b3ecbe01a722bc88e60d93ea4cd53f791
ranigarad <ranigarad2017@gmail.com>
2026-03-09 16:57:05 +0530

    first commit

---
+++ BankingServices/src/main/java/com/example/demo/NetBanking.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NetBanking {
+	
+	@GetMapping("bank/checkBal/{acNo}/{pin}")
+	String	checkBalance(@PathVariable int acNo,@PathVariable int pin)
+		{
+			 if(pin<1000)
+			 {
+				 
+				 return "pin is invalid";
+			 }
+			
+			return "u r bal is 10000";
+			
+		}
+		
+		
+
+}
